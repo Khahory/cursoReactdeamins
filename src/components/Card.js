2969094c3daf48d0d1fc6a77bd131bb4df3fcf4e
Khahory <angeld05@live.com>
2021-08-07 23:00:27 +0200

    La para

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,24 @@
 import React from "react";
 import exeImg from '../images/exercise.png'
 import './styles/Card.css'
+import circuleImg from '../images/circles.png'
 
 class Card extends React.Component {
     render() {
         return (
-            <div className='card mx-auto Fitness-Card'>
+            <div className='card mx-auto Fitness-Card'
+            style={{
+                backgroundImage: `url(${circuleImg}), linear-gradient(to right, ${this.props.leftColor}, ${this.props.rightColor})`
+            }}
+            >
                 <div className='card-body'>
                     <div className='row center'>
                         <div className='col-6'>
                             <img src={exeImg} alt="" className='float-right'/>
                         </div>
                         <div className='col-6 Fitness-Card-Info'>
-                            <h1>Titulo</h1>
-                            <p>Esto es una prueba vacana</p>
+                            <h1>{this.props.title}</h1>
+                            <p>{this.props.description}</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -6,5 +6,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root')
 
 ReactDOM.render(
-    <Card />, container
-)
+    <Card
+        title={'Titulo dinamic'}
+        description={'descriptiom dinamic'}
+        img={'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'}
+        leftColor="#A74CF2"
+        rightColor="#617BFB"
+    />, container )
