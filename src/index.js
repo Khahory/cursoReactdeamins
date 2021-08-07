1c7fa97fb9d606f69cbc3fb27ad88221b923074e
Khahory <angeld05@live.com>
2021-08-08 01:00:12 +0200

    Ahora muestra paginas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/Card'
-import Welcome from "./components/Welcome";
 import 'bootstrap/dist/css/bootstrap.css'
+import Exercises from './pages/Exercises'
 
 const container = document.getElementById('root')
 
 ReactDOM.render(
-    <div>
-        <Welcome name='Khahory'/>
-        <Card
-            title={'Titulo dinamic'}
-            description={'descriptiom dinamic'}
-            img={'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'}
-            leftColor="#A74CF2"
-            rightColor="#617BFB"
-        />
-    </div>
+    <Exercises />
     , container )
