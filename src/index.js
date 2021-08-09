1d465986b608ca6abacc1af9c5a7439f70610d2c
Khahory <angeld05@live.com>
2021-08-09 17:11:09 +0200

    Api separada

--- src/index.js
+++ src/index.js
@@ -8,3 +8,8 @@ const container = document.getElementById('root')
 ReactDOM.render(
     <Exercises />
     , container )
+
+
+
+
+// https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/data.json?alt=media&token=08c545aa-6074-4ff6-9405-6080e6eb5a71
