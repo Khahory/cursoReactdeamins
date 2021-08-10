19f0508d6a4651b44ca3c50478be18ec048e0015
Khahory <angeld05@live.com>
2021-08-10 15:01:33 +0200

    ATENTO Estas compartiendo informacion a un componente hermano (CARD) mediante un componente padre (EXERCISES_NEW)
    Esto vale hora, cuando puedas lo ves y lo estudias cuando lo requieras

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,39 +4,24 @@ import circuleImg from '../images/circles.png'
 
 class Card extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            imagenState: 'https://i.ytimg.com/vi/oMesfTZFaeM/hqdefault.jpg'
-        }
-    }
-
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                imagenState: this.props.img
-            })
-        }, 1000)
-    }
-
     render() {
         // Ejemplo para que puedas mejorar el codigo sin poner this
-        const { title } = this.props
+        const { title, img, leftColor, rightColor, description } = this.props
 
         return (
             <div className='card mx-auto Fitness-Card'
             style={{
-                backgroundImage: `url(${circuleImg}), linear-gradient(to right, ${this.props.leftColor}, ${this.props.rightColor})`
+                backgroundImage: `url(${circuleImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
             }}
             >
                 <div className='card-body'>
                     <div className='row center'>
                         <div className='col-6'>
-                            <img src={this.state.imagenState} alt="" className='float-right'/>
+                            <img src={img} alt="" className='float-right'/>
                         </div>
                         <div className='col-6 Fitness-Card-Info'>
                             <h1>{title}</h1>
-                            <p>{this.props.description}</p>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
