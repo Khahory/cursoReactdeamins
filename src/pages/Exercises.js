7533f44db9fe880b0992ad823bc4eff4f4bf96d3
Khahory <angeld05@live.com>
2021-08-11 19:40:17 +0200

    muestra error si hay problemas con la db

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -2,13 +2,17 @@ import React from "react";
 import ExercisesList from '../components/ExercisesList'
 import Welcome from "../components/Welcome";
 import AddButtom from '../components/AddButton'
+import Loading from "../components/Loading";
+import FatalError from '../pages/500'
 
 class Exercises extends React.Component{
 
     //Gracias a Babel no requerimos inicializar los STATES dentro del constructor
     //Ya lo hara por nosotros si solo ponemos de objeto padra la variable llamada STATE
     state = {
-        data: []
+        data: [],
+        loading: true,
+        error: null
     }
 
     //Llamada api get
@@ -17,15 +21,30 @@ class Exercises extends React.Component{
     }
 
     fetchExercises = async () => {
-        let res = await fetch('http://localhost:8000/api/exercises/')
-        let data = await res.json()
+        try {
+            let res = await fetch('http://localhost:8000/api/exercises/')
+            let data = await res.json()
 
-        this.setState({
-            data
-        })
+            this.setState({
+                data,
+                loading: false
+            })
+        } catch (error) {
+            this.setState({
+                loading: false,
+                //Sepasara automaticamente como si fuera error: error(este es del parametro)
+                error
+            })
+        }
     }
 
     render() {
+        if (this.state.loading)
+            return <Loading/>
+
+        if (this.state.error)
+            return <FatalError/>
+        
         return (
             <div>
                 <Welcome name='Khahory'/>
