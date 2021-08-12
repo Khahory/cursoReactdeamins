0b529d4573a31ee8e1779d683f0bad3dd6ca14f4
Khahory <angeld05@live.com>
2021-08-12 19:57:46 +0200

    Mi hook personalizado, sirve mucho para reutilizar codigo con el de una api

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import React, {useState,useEffect} from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchGenerico = async () => {
+            try {
+                let res = await fetch(url)
+                let data = await res.json()
+                setData(data)
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+                setError(error)
+            }
+        }
+        fetchGenerico()
+    }, [url])
+
+    return { data, loading, error }
+}
+
+export default useFetch
