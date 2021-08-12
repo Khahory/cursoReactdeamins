838c04283e0534f1af9738b5ffeeac64d48efc63
Khahory <angeld05@live.com>
2021-08-12 18:37:35 +0200

    trabajando con hooks Pogers

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,5 +1,5 @@
 // Vamos a probar los hooks
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "../components/Loading";
 import FatalError from '../pages/500'
 import Exercises from "./Exercises";
@@ -8,6 +8,21 @@ const ExercisesContainer = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchExercises = async () => {
+            try {
+                let res = await fetch('http://localhost:8000/api/exercises/')
+                let data = await res.json()
+                setData(data)
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+                setError(error)
+            }
+        }
+        fetchExercises()
+    }, [])
 
     if (loading)
         return <Loading/>
