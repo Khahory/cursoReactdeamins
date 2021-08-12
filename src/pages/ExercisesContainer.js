fe77128194357a7c573ec9a392f9592af15847c6
Khahory <angeld05@live.com>
2021-08-12 18:24:54 +0200

    trabajando con hooks

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,51 +1,69 @@
-import React from "react";
+// Vamos a probar los hooks
+import React, { useState } from "react";
 import Loading from "../components/Loading";
 import FatalError from '../pages/500'
 import Exercises from "./Exercises";
 
-class ExercisesContainer extends React.Component{
+const ExercisesContainer = () => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
-    //Gracias a Babel no requerimos inicializar los STATES dentro del constructor
-    //Ya lo hara por nosotros si solo ponemos de objeto padra la variable llamada STATE
-    state = {
-        data: [],
-        loading: true,
-        error: null
-    }
+    if (loading)
+        return <Loading/>
 
-    //Llamada api get
-    async componentDidMount() {
-        await this.fetchExercises()
-    }
+    if (error)
+        return <FatalError/>
 
-    fetchExercises = async () => {
-        try {
-            let res = await fetch('http://localhost:8000/api/exercises/')
-            let data = await res.json()
-
-            this.setState({
-                data,
-                loading: false
-            })
-        } catch (error) {
-            this.setState({
-                loading: false,
-                //Sepasara automaticamente como si fuera error: error(este es del parametro)
-                error
-            })
-        }
-    }
-
-    render() {
-        if (this.state.loading)
-            return <Loading/>
-
-        if (this.state.error)
-            return <FatalError/>
-        
-        return <Exercises
-            data={this.state.data}/>
-    }
+    return <Exercises
+        data={data}/>
 }
 
 export default ExercisesContainer
+
+// class ExercisesContainer extends React.Component{
+//
+//     //Gracias a Babel no requerimos inicializar los STATES dentro del constructor
+//     //Ya lo hara por nosotros si solo ponemos de objeto padra la variable llamada STATE
+//     state = {
+//         data: [],
+//         loading: true,
+//         error: null
+//     }
+//
+//     //Llamada api get
+//     async componentDidMount() {
+//         await this.fetchExercises()
+//     }
+//
+//     fetchExercises = async () => {
+//         try {
+//             let res = await fetch('http://localhost:8000/api/exercises/')
+//             let data = await res.json()
+//
+//             this.setState({
+//                 data,
+//                 loading: false
+//             })
+//         } catch (error) {
+//             this.setState({
+//                 loading: false,
+//                 //Sepasara automaticamente como si fuera error: error(este es del parametro)
+//                 error
+//             })
+//         }
+//     }
+//
+//     render() {
+//         if (this.state.loading)
+//             return <Loading/>
+//
+//         if (this.state.error)
+//             return <FatalError/>
+//
+//         return <Exercises
+//             data={this.state.data}/>
+//     }
+// }
+//
+// export default ExercisesContainer
